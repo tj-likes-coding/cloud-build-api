f531385d3a559f6d9bd62d43cc1144d0969220be
Tejas Nayak <101463222+tj-likes-coding@users.noreply.github.com>
2023-03-07 14:29:16 +0530

    Update index.js

--- fetch/index.js
+++ fetch/index.js
@@ -1,4 +1,16 @@
 const fs = require("fs");
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "sql.freedb.tech",
+  user: "freedb_cloud-build-author",
+  password: "Km5pdq!KCYyAN*c",
+  database: "freedb_cloud-build-js"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+});
 
 exports.fetchGit = (req, res) => {
     let pkgname = req.headers['package_name'];
@@ -39,12 +51,25 @@ exports.fetchFileList = (req, res) => {
     let pkgname = req.headers['package_name'];
     let author = req.headers['author'];
     let appname = req.headers['appname'];
-    let json = fs.readFileSync("../db.txt", "utf-8", (data) => {return data;});
-    json = JSON.parse(json);
-    res.json(json);
-    let data = json[pkgname];
+//     let json = fs.readFileSync("../db.txt", "utf-8", (data) => {return data;});
+//     json = JSON.parse(json);
+//     res.json(json);
+//     let data = json[pkgname];
     
-    if( data.author === author && data.appname === appname ) {
-        res.json(data.files);
-    }
+//     if( data.author === author && data.appname === appname ) {
+//         res.json(data.files);
+//     }
+    
+    let results;
+    
+    con.query("SELECT * FROM `cloud-build-repos`", function (err, result) {
+    if (err) throw err;
+        results = result;
+    });
+    
+    results.forEach(row => {
+        if(row['package_name'] === pkgname && row['appname'] === appname && row['author'] === author) {
+            res.json(row);
+        }
+    });
 }
