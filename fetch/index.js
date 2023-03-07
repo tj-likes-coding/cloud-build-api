9e0cf696fa7ce324cc47433a97a48ce42375e77c
Tejas Nayak <101463222+tj-likes-coding@users.noreply.github.com>
2023-03-07 12:48:07 +0530

    Add files via upload

---
+++ fetch/index.js
@@ -0,0 +1,46 @@
+exports.fetchGit = (req, res) => {
+    let pkgname = req.headers['package_name'];
+    let author = req.headers['author'];
+    let appname = req.headers['appname'];
+    let json = fs.readFileSync("../db.txt", "utf-8", (data) => {return data;});
+    json = JSON.parse(json);
+    let data = json[pkgname];
+
+    if( data.author === author && data.appname === appname ) {
+        fetch(data.repo+"/"+data.main).then((response) => {
+            res.json({
+                data: response.text()
+            });
+        });
+    }
+}
+
+exports.fetchGitFile = (req, res) => {
+    let pkgname = req.headers['package_name'];
+    let author = req.headers['author'];
+    let appname = req.headers['appname'];
+    let json = fs.readFileSync("../db.txt", "utf-8", (data) => {return data;});
+    json = JSON.parse(json);
+    let data = json[pkgname];
+    
+    if( data.author === author && data.appname === appname ) {
+        fetch(data.repo+"/"+req.params.filename).then((response) => {
+            res.json({
+                data: response.text()
+            });
+        });
+    }
+}
+
+exports.fetchFileList = (req, res) => {
+    let pkgname = req.headers['package_name'];
+    let author = req.headers['author'];
+    let appname = req.headers['appname'];
+    let json = fs.readFileSync("../db.txt", "utf-8", (data) => {return data;});
+    json = JSON.parse(json);
+    let data = json[pkgname];
+    
+    if( data.author === author && data.appname === appname ) {
+        res.json(data.files);
+    }
+}
