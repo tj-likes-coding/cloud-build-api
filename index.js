80bde367275a29cb57c8e0b204f420c075db5703
Tejas Nayak <101463222+tj-likes-coding@users.noreply.github.com>
2023-03-07 12:50:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');const bodyParser = require('body-parser');
+const fs = require("fs");
+const { fetchGit, fetchGitFile } = require('./fetch');
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.route("/fetch/:filename")
+    .get((req, res) => {
+        if(req.headers['kindred'] !== "cloud-build") {
+            res.statusCode = 404;
+            res.end();
+        }
+        fetchGitFile(req, res);
+    });
+
+app.route("/fetch")
+    .get((req, res) => {
+        if(req.headers['kindred'] !== "cloud-build") {
+            res.statusCode = 404;
+            res.end();
+        }
+        fetchGit(req, res);
+    })
+
+app.listen(port, () => {  console.log('We are live on ' + port);});
